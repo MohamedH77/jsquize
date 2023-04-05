e31437a188d91115263717ccb09e0cbf568d9672
MohamedH77 <amiini44@hotmail.com>
2023-04-05 09:51:16 -0500

    jsupdats

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,104 @@
-var startButton = document.querySelector();
-var timerCount = document.querySelector();
-var questions = [""];
-var secondsRemaining = 1111;
-var gameInProgross = false;
+var startButton = document.querySelector("#button");
+// var timerCount = document.querySelector();
+// var secondsRemaining = 1111;
+// var gameInProgross = false;
 
-function timerCount(){
-    var countdown = setInterval(function(){
-        secondsRemaining--;
-        timerCount.textContent = secondsRemaining;
-        if( secondsRemaining <= 0){
-            endGame()
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above",
+        ],
+        answer: "all of the above",
+    },
+    {
+        title:
+            "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
+    },
+    {
+        title:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
+];
+
+function showQuestion(){
+    startButton.addEventListener("click", showQuestionOne)
+}
+
+var i = 0
+//function to show question 1
+function showQuestionOne(){
+    document.getElementById('titles').innerHTML =
+    `<p>${questions[i].title}</p>`
+
+    document.getElementById('choices').innerHTML =
+    `
+    <button>${questions[i].choices[0]}</button>
+    <button>${questions[i].choices[1]}</button>
+    <button>${questions[i].choices[2]}</button>
+    <button>${questions[i].choices[3]}</button>
+    `
+
+}
+
+function checkAnswer(){
+    document.querySelector("#choices").addEventListener("click", function(e){
+            var choice = e.target.innerText
+            console.log(choice)
+            if(choice  == questions[0].answer){
+                console.log('correct')
+                document.getElementById('checkright').innerHTML=
+                "correct!"
+            }else{console.log('wrong')
+            document.getElementById('checkright').innerHTML=
+                "wrong!"
         }
-
-    },1000)
-    gameInProgross = true;
+        i++
+        showQuestionOne()
+    })
 }
-
-document.body.addEventListener("keydown", function(event)){
-
+if{
+    
 }
+checkAnswer()
+//function to check answer
+
+
+showQuestion()
+
+
+// function timerCount(){
+//     var countdown = setInterval(function(){
+//         secondsRemaining--;
+//         timerCount.textContent = secondsRemaining;
+//         if( secondsRemaining <= 0){
+//             endGame()
+//         }
+
+//     },1000)
+//     gameInProgross = true;
+// }
+
+// document.body.addEventListener("keydown", function(event)){
+
+// }
 
 
 
